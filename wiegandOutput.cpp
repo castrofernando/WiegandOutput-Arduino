759b3e63ce31c3c3f217010b5942750e54382db3
Fernando Castro <fernando.castro@acess-e.com>
2021-07-07 14:02:29 -0300

    fixed parity bits. Even and Odd was inverted position

--- wiegandOutput.cpp
+++ wiegandOutput.cpp
@@ -1,4 +1,3 @@
-
 #include <Arduino.h>
 #include "wiegandOutput.h"
 #if (defined(__AVR__))
@@ -48,22 +47,24 @@ void WiegandOut::createParity(unsigned long data, unsigned int bits, bool useFac
 
     //34 bits -> use 32 to check parity ((34-2)/2) = 16 even / 16 odd
     //26 bits -> use 24 to check parity ((26-2)/2) = 12 even / 12 odd
-    for (int i = 0; i < (bits-2)/2; i++)
+    int b = (bits-2)/2;
+    for (int i = 0; i < b; i++)
     {
+
         tempData = data>>i;
         if(tempData & 0x01){        //this count the qty of 1 and do a xor to complete the even parity;            
-            _evenParity = _evenParity ^ 1;           
+            _oddParity = _oddParity ^ 1;           
         }
     }
     
     //34 bits -> use 32 to check parity ((34-2)/2) = 16 even / 16 odd
     //26 bits -> use 24 to check parity ((26-2)/2) = 12 even / 12 odd
     tempData=0;
-    for (int i = (bits-2)/2; i < bits; i++)
+    for (int i = b; i < bits-2; i++)
     {
         tempData = data>>i;
         if(tempData & 0x01){        //this count the qty of 1 and do a xor to complete the odd parity;
-            _oddParity = _oddParity ^ 1;
+            _evenParity = _evenParity ^ 1;           
         }
     }
 
@@ -75,7 +76,7 @@ void WiegandOut::sendD0(){
     _delay_us(DELAY_PULSE_SHORT);
     digitalWrite(_pinData0,HIGH);
     _delay_us(DELAY_PULSE_LONG);
-    //Serial.print("0");
+    Serial.print("0");
 }
 
 /*Send D1 pin pulse*/
@@ -84,7 +85,7 @@ void WiegandOut::sendD1(){
     _delay_us(DELAY_PULSE_SHORT);
     digitalWrite(_pinData1,HIGH);
     _delay_us(DELAY_PULSE_LONG);
-    //Serial.print("1");
+    Serial.print("1");
 }
 
 /*Send wiegand data
@@ -132,5 +133,5 @@ void WiegandOut::send(unsigned long data, unsigned int bits, bool useFacilityCod
     }
     //send odd parity bit
     if(_oddParity){sendD1();}else{sendD0();}
-    
+    Serial.println();
 }
